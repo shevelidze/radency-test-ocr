3f1e014ae3dea65a8c0666e5cf649eb34d1a9793
Denys Shevel <denys.shevel16@gmail.com>
2022-11-14 20:59:18 +0100

    Finish the web api

--- web-api/src/app.service.ts
+++ web-api/src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+import { OCR_CLIENT_SERVICE, FILE_OCR_EVENT_PATTERN } from './app.constansts';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject(OCR_CLIENT_SERVICE) private readonly ocrClientService: ClientProxy,
+  ) {}
+
+  sendFileToQueue(fileUrl: string) {
+    this.ocrClientService.emit(FILE_OCR_EVENT_PATTERN, fileUrl);
   }
 }
